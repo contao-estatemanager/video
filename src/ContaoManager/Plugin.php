cf93341c71ab3e8ef7a6e8a6df174667157bee28
oveleon-doi <daniele@oveleon.de>
2019-02-08 09:47:36 +0100

    Initial commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Oveleon ImmoManager.
+ *
+ * @link      https://github.com/oveleon/contao-immo-manager-bundle
+ * @copyright Copyright (c) 2018-2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://github.com/oveleon/contao-immo-manager-bundle/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Oveleon\ContaoImmoManagerVideoBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Oveleon\ContaoImmoManagerVideoBundle\ContaoImmoManagerVideoBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ContaoImmoManagerVideoBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['immo-manager-video']),
+        ];
+    }
+}
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file is part of Oveleon ImmoManager.
+ *
+ * @link      https://github.com/oveleon/contao-immo-manager-bundle
+ * @copyright Copyright (c) 2018-2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://github.com/oveleon/contao-immo-manager-bundle/blob/master/LICENSE
+ */
