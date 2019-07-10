15256a62eea59fe63cd34772500e67de581ea684
oveleon <fabian@oveleon.de>
2019-07-10 15:49:35 +0200

    Fixed blocked database update without license.

--- src/EstateManagerVideo.php
+++ src/EstateManagerVideo.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * This file is part of Oveleon ImmoManager.
+ * This file is part of Contao EstateManager.
  *
- * @link      https://github.com/oveleon/contao-immo-manager-bundle
- * @copyright Copyright (c) 2018-2019  Oveleon GbR (https://www.oveleon.de)
- * @license   https://github.com/oveleon/contao-immo-manager-bundle/blob/master/LICENSE
+ * @link      https://www.contao-estatemanager.com/
+ * @source    https://github.com/contao-estatemanager/virtual-tour
+ * @copyright Copyright (c) 2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
  */
 
 declare(strict_types=1);
 
 
-
-namespace Oveleon\ContaoImmoManagerVideoBundle;
+namespace ContaoEstateManager\Video;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
